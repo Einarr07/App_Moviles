7afd05ca00275c0c3d261e5a2063d82d58cb1355
unknown <aidenperse@gmail.com>
2024-01-03 14:16:42 -0500

    cambios

---
+++ ionic-geolocation-app/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyDsxerCFpDEXthHXWi-xr0zYY2mUuO6rMo",
+    authDomain: "moviles-ec43a.firebaseapp.com",
+    databaseURL: "https://moviles-ec43a-default-rtdb.firebaseio.com",
+    projectId: "moviles-ec43a",
+    storageBucket: "moviles-ec43a.appspot.com",
+    messagingSenderId: "170362843768",
+    appId: "1:170362843768:web:786dc0f49aac202370738d",
+    measurementId: "G-L5XHLL9Y9X"
+  },
+  production: true
+};
